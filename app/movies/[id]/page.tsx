8e445714dc24127f6caa75d59cd5ddd8b3ddbf4b
twy8939 <twy8939@naver.com>
2024-08-16 22:58:26 +0900

    feat : Add generateMetadata

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,13 +1,20 @@
-"use client";
-
-import { useParams } from "next/navigation";
 import UI from "./ui";
 import { getMovie } from "actions/movieActions";
 
-export default async function MovieDetail() {
-  const { id } = useParams();
+export async function generateMetadata({ params, searchParams }) {
+  const movie = await getMovie(params.id);
 
-  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+    description: movie.overview,
+    openGraph: {
+      images: [movie.image_url],
+    },
+  };
+}
+
+export default async function MovieDetail({ params }) {
+  const movie = await getMovie(params.id);
 
   return (
     <main className="flex items-center bg-blue-50 w-full absolute top-0 bottom-0 left-0 right-0">
